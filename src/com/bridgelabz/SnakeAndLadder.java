1dcb58c31b1dc88c2d9eb80667adbb6bcfaefa9a
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-09-15 13:48:38 +0530

    Second UC Case

--- src/com/bridgelabz/SnakeAndLadder.java
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -1,10 +1,15 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class SnakeAndLadder {
 
         public static void main(String[] args) {
             System.out.println("Welcome to Snake and Ladder Game");
             int startPosition = 0;
+            Random random = new Random();
+            int diceNumber = random.nextInt(6)+1;
+            System.out.println("Dice Number : " + diceNumber);
         }
 }
 
