15f96701e14e1e659cc0a5e06c628620ac6b0339
ajayjadhav2208 <ajayjadhav22081997@gmail.com>
2021-09-15 13:37:43 +0530

    First UC Case

---
+++ src/com/bridgelabz/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class SnakeAndLadder {
+
+        public static void main(String[] args) {
+            System.out.println("Welcome to Snake and Ladder Game");
+            int startPosition = 0;
+        }
+}
+
